ab5d635710b2f280942abb4d5abf9e41452ab8e3
Khaliq <61682259+khaliq8145@users.noreply.github.com>
2024-11-29 15:11:56 +0700

    Update and rename infinity_sunfish.mk to lineage_sunfish.mk

--- lineage_sunfish.mk
+++ lineage_sunfish.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/sunfish/aosp_sunfish.mk)
@@ -15,30 +15,12 @@ include device/google/sunfish/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4a
-PRODUCT_NAME := infinity_sunfish
+PRODUCT_NAME := lineage_sunfish
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Whether you are compiling being an OFFICIAL Maintainer:
-INFINITY_BUILD_TYPE := UNOFFICIAL
-
-# Maintainer Name
-INFINITY_MAINTAINER := Bias_khaliq
-
-# Whether the package includes System BLURS
-TARGET_SUPPORTS_BLUR := true
-
-# Whether the compiled package ships Widely Used Minimal Google Apps:
-WITH_GAPPS := true
-
-# Whether the compiled package ships Complete present Google Apps:
-TARGET_SHIPS_FULL_GAPPS := true
-
-# Whether the compiled shipped gapps package uses Google Dialer, Messaging, Contacts:
-TARGET_BUILD_GOOGLE_TELEPHONY := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sunfish-user 13 TQ3A.230805.001.S1 10786265 release-keys" \
