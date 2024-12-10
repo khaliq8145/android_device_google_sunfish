cabdba1e0a361233a81bc95436d249ab1a2b0cb5
Khaliq <61682259+khaliq8145@users.noreply.github.com>
2024-12-10 18:39:10 +0700

    Update device-common.mk

--- device-common.mk
+++ device-common.mk
@@ -65,7 +65,7 @@ persist.vendor.bt.aac_frm_ctl.enabled=true
 # Set lmkd options
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.config.low_ram ?= false \
-	ro.lmk.log_stats = false \
+	ro.lmk.log_stats = true \
 
 # charger
 PRODUCT_PRODUCT_PROPERTIES += \
